d96d30afe28b838d58cf2cf4b91dd9d7990d6e4f
Kirill <opoxmeloid@yandex.ru>
2023-06-20 10:12:34 +0300

    change adoptation for phone screen

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import clsx from 'clsx'
 
 export default function App() {
   return (
-    <div className='grid h-[100vh] place-items-center dark:border-neutral-600 dark:bg-neutral-800'>
-      <div className={clsx('h-[812px] w-[375px]')}>
-        <TestFold />
-      </div>
+    // <div className='grid h-[100vh] place-items-center dark:border-neutral-600 dark:bg-neutral-800'>
+    //   <div className={clsx('h-[812px] w-[375px]')}>
+    //     <TestFold />
+    //   </div>
+    // </div>
+    <div className='h-[100vh] w-[100vw] overflow-y-hidden'>
+      <TestFold />
     </div>
   )
 }
